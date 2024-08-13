c20971adddc187355755dfb66fa2994badfe9a93
oochie-ch <oochiiee.c@gmail.com>
2024-08-13 17:12:46 +0900

    재저장

--- screen_set.py
+++ screen_set.py
@@ -14,30 +14,3 @@ def show_menu():
       5. 프로그램 종료
       '''
     )
-
-
-def main():
-    
-    """Main function to run the banking system."""
-    while True:
-        show_menu()
-        choice = menu_input()
-        print()
-
-        if choice == 1:
-            make_account()
-        elif choice == 2:
-            deposit_money()
-        elif choice == 3:
-            withdraw_money()
-        elif choice == 4:
-            show_all_account_information()
-        elif choice == 5:
-            print("프로그램을 종료합니다.")
-            break
-        else:
-            print("Illegal selection..")
-            print()
-
-if __name__ == "__main__":
-    main()
